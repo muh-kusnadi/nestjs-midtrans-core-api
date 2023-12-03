4b8f440550483ff2cfa2af1aa8e24135881a5624
muh-kusnadi <muh.kusnadi99@gmail.com>
2023-12-03 11:09:56 +0800

    update credit card response logic and test

--- src/charge/charge.controller.ts
+++ src/charge/charge.controller.ts
@@ -1,17 +1,22 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { ChargeService } from './charge.service';
 import { BaseChargeDto } from './dto/baseCharge.dto';
+import { Response } from 'express';
 
 @Controller('charge')
 export class ChargeController {
   constructor(private chargeService: ChargeService) {}
 
   @Post()
-  @HttpCode(200)
-  async createCharge(@Body() baseChargeDto: BaseChargeDto) {
-    return this.chargeService.executeCharge(
+  async createCharge(
+    @Body() baseChargeDto: BaseChargeDto,
+    @Res() response: Response,
+  ) {
+    const result = await this.chargeService.executeCharge(
       baseChargeDto.paymentMethod,
       baseChargeDto,
     );
+
+    response.status(result.status_code).send(result);
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -34,7 +34,28 @@ describe('App e2e', () => {
     describe('CreditCardPaymentStrategy', () => {
       it('should process credit card payment successfully', async () => {
         mockedAxios.post.mockResolvedValueOnce({
-          data: { success: true, message: 'Payment processed' },
+          data: {
+            status_code: 201,
+            status_message: 'Success, Credit Card transaction is successful',
+            channel_response_code: '00',
+            channel_response_message: 'Approved',
+            bank: 'bni',
+            eci: '05',
+            transaction_id: '405d27d5-5ad9-43ac-bdd6-0ccbde7d7dda',
+            order_id: 'test-transaction-54321',
+            merchant_id: 'G490526303',
+            gross_amount: '789000.00',
+            currency: 'IDR',
+            payment_type: 'credit_card',
+            transaction_time: '2023-12-12 15:50:54',
+            transaction_status: 'capture',
+            fraud_status: 'accept',
+            approval_code: '1597223068747',
+            masked_card: '48111111-1114',
+            card_type: 'credit',
+            three_ds_version: '2',
+            challenge_completion: true,
+          },
         });
 
         await pactum
@@ -49,7 +70,7 @@ describe('App e2e', () => {
             email: 'john.doe@example.com',
             phone: '6234738473874',
           })
-          .expectStatus(200);
+          .expectStatus(201);
       });
     });
   });
